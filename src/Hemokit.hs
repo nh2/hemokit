d22c65d5256b39ae3120a0de0c498f8f5a4abf37
Patrick Chilton <chpatrick@gmail.com>
2013-06-01 14:26:09 +0100

    Use built-in HIDAPI device filtering.

--- src/Hemokit.hs
+++ src/Hemokit.hs
@@ -255,7 +255,7 @@ data EmotivDevice = EmotivDevice
   }
 
 getEmotivDevices :: IO [EmotivDeviceInfo]
-getEmotivDevices = map EmotivDeviceInfo . filter isEmotivDevice <$> HID.enumerate Nothing Nothing
+getEmotivDevices = map EmotivDeviceInfo <$> HID.enumerate (Just _EMOTIV_VENDOR_ID) (Just _EMOTIV_PRODUCT_ID)
 
 
 openEmotivDevice :: EmotivDeviceInfo -> IO EmotivDevice
