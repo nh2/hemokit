737c86473358e0685c6334bd30c6e530e1aa946a
Niklas Hambüchen <mail@nh2.me>
2013-06-12 00:59:50 +0800

    Improve headmap. Still not done but it now displays qualities

--- apps/Headmap.hs
+++ apps/Headmap.hs
@@ -1,53 +1,82 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 module Main where
 
+import Control.Concurrent
+import Control.Monad
+import Data.Vector ((!))
+-- import Data.IORef
+-- import qualified Data.Vector as V
 import Graphics.Rendering.Cairo
 import Graphics.UI.Gtk
 -- import Graphics.UI.Gtk.Gdk.EventM
 import Graphics.Rendering.Cairo.SVG
 
+import Hemokit
+
+
 main :: IO ()
 main = do
   svg <- svgNewFromFile "images/head-map.svg"
   let (width, height) = svgGetSize svg
-      sensorL x bottomY name = do
-        let y = fromIntegral height - bottomY
-        setRed   >> drawCircle x y 15
-        TextExtents{ textExtentsWidth = w } <- textExtents name
-        moveTo (x - 8 - w/2) (y - 8)
-        setBlack >> drawStr name
-      sensorR rightX bottomY name = do
-        let y = fromIntegral height - bottomY
-            x = fromIntegral width - rightX
+      drawSensor (xi, yi) _sensor qual = do
+        let (x, y) = (fromIntegral xi, fromIntegral yi)
+            -- name   = show sensor ++ " " ++ show qual
+            name   = show qual
         setRed   >> drawCircle x y 15
         TextExtents{ textExtentsWidth = w } <- textExtents name
         moveTo (x - 8 - w/2) (y - 8)
         setBlack >> drawStr name
 
-  run width height $ do
+  emotivStateMvar <- newEmptyMVar
+  forkIO $ do
+    putMVar emotivStateMvar undefined
+    putStrLn "Waiting for EEG data..."
+    withDataFromLastEEG Consumer (void . swapMVar emotivStateMvar . fst)
+
+  run width height $ \window -> do
     _ <- svgRender svg
+    liftIO $ flush
 
-    -- Left side
-    sensorL 125 434 "AF3" -- AF3
-    sensorL 145 380 "F3"  -- F3
-    sensorL  77 360 "F7"  -- F7
-    sensorL 107 318 "FC5" -- FC5
-    sensorL  55 250 "T7"  -- T7
-    sensorL  51 178 "CMS" -- CMS low
-    sensorL 102 153 "CMS" -- CMS high
-    sensorL  80  93 "P7"  -- P7
-    sensorL 147  35 "O1"  -- O1
+    EmotivState{ counter, qualities } <- liftIO $ readMVar emotivStateMvar
+    liftIO $ print (counter, qualities)
 
-    sensorR 125 434 "AF4" -- AF4
-    sensorR 145 380 "F4"  -- F4
-    sensorR  77 360 "F7"  -- F7
-    sensorR 107 318 "FC6" -- FC6
-    sensorR  55 250 "T8"  -- T8
-    sensorR  51 178 "DRL" -- DRL low
-    sensorR 102 153 "DRL" -- DRL high
-    sensorR  80  93 "P8"  -- P8
-    sensorR 147  35 "O2"  -- O2
+    forM_ allSensors $ \s ->
+      drawSensor (sensorPosition (width, height) s) s (qualities ! (fromEnum s))
 
-    return ()
+    liftIO $ flush >> widgetQueueDraw window
+
+
+sensorPosition :: (Int, Int) -> Sensor -> (Int, Int)
+sensorPosition (width, height) sensor = case sensor of
+
+  -- Left side
+  AF3 -> ( 125 , height - 434 )
+  F3  -> ( 145 , height - 380 )
+  F7  -> (  77 , height - 360 )
+  FC5 -> ( 107 , height - 318 )
+  T7  -> (  55 , height - 250 )
+  -- CMS -> (  51 , height - 178 )
+  -- F7  -> (  51 , height - 178 )
+  -- CMS -> ( 102 , height - 153 )
+  P7  -> (  80 , height -  93 )
+  O1  -> ( 147 , height -  35 )
+
+  -- Right side
+  AF4 -> yMirror AF3
+  F4  -> yMirror F3
+  F8  -> yMirror F7
+  FC6 -> yMirror FC5
+  T8  -> yMirror T7
+  -- DRL -> yMirror CMS
+  -- F8  -> yMirror F7
+  -- DRL -> yMirror CMS
+  P8  -> yMirror P7
+  O2  -> yMirror O1
+  where
+    yMirror s = let (x,y) = sensorPosition (width, height) s
+                 in (width - x, y)
+
 
 
 drawCircle :: Double -> Double -> Double -> Render ()
@@ -59,23 +88,29 @@ drawCircle x y radius = do
 
 
 
-run :: Int -> Int -> Render () -> IO ()
+run :: Int -> Int -> (Window -> Render ()) -> IO ()
 run width height renderAct = do
   _ <- initGUI
-  dia <- dialogNew
-  _ <- dialogAddButton dia stockClose ResponseClose
-  contain <- dialogGetUpper dia
+  win <- windowNew
+  vbox <- vBoxNew False 0
+  closeButton <- buttonNewWithLabel "Close"
+  onClicked closeButton (widgetDestroy win)
   canvas <- drawingAreaNew
   _ <- canvas `onSizeRequest` return (Requisition width height)
-  _ <- canvas `on` exposeEvent $ tryEvent $ updateCanvas canvas renderAct
-  boxPackStartDefaults contain canvas
+  _ <- canvas `on` exposeEvent $ tryEvent $ updateCanvas canvas (renderAct win)
+
+  containerAdd vbox closeButton
+  containerAdd vbox canvas
+  containerAdd win vbox
+
   widgetShow canvas
-  _ <- dialogRun dia
-  widgetDestroy dia
+  widgetShowAll win
+  onDestroy win mainQuit
+  -- flush
+  mainGUI
   -- Flush all commands that are waiting to be sent to the graphics server.
   -- This ensures that the window is actually closed before ghci displays the
   -- prompt again.
-  flush
 
   where
     updateCanvas :: DrawingArea -> Render () -> EventM EExpose ()
