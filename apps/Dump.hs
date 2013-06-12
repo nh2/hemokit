4684907d9a6b43e3ac4cee9a90c09e493967c362
Niklas Hambüchen <mail@nh2.me>
2013-06-13 00:30:29 +0800

    Dump: Add command line parser, allow reading from device file

--- apps/Dump.hs
+++ apps/Dump.hs
@@ -1,26 +1,72 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 module Main where
 
 import           Control.Monad
+import           Options.Applicative
 import           System.IO
-import           System.Environment
 import           Text.Show.Pretty (ppShow)
 
-import           Hemokit
+import           Hemokit hiding (serial)
+
+
+data DumpArgs = DumpArgs
+  { model       :: EmotivModel
+  , serial      :: Maybe SerialNumber
+  , packetsOnly :: Bool
+  , fromFile    :: Maybe FilePath
+  -- , bla         :: FilePath
+  } deriving (Eq, Show)
+
+parseModel :: Monad m => String -> m EmotivModel
+parseModel s = case s of
+  "consumer"  -> return Consumer
+  "developer" -> return Developer
+  _           -> fail "Model is not valid. Must be 'consumer' or 'developer'."
+
+
+dumpArgs :: Parser DumpArgs
+dumpArgs = DumpArgs
+  <$> nullOption
+      ( long "model" <> metavar "MODEL"
+        <> reader parseModel <> value Consumer
+        <> help "Consumer or Developer model" )
+  <*> (optional . nullOption)
+      ( long "serial" <> metavar "SERIALNUMBER"
+        <> maybeReader makeSerialNumberFromString "Serial number of has invalid format"
+        <> help "The serial to use. If no --from-file is given, this will select the device" )
+  <*> switch
+      ( long "packets-only"
+        <> help "Dump packets as sent from the device instead of cumulative state" )
+  <*> (optional . strOption)
+      ( long "from-file" <> metavar "PATH"
+        <> help "The file path to read from (e.g. /dev/hidraw0 or myfile.dump)" )
+  -- <*> strOption
+  --     ( long "from-filea" <> metavar "PATH"
+  --       <> help "The file path to read from (e.g. /dev/hidraw0 or myfile.dump)" )
+  where
+    maybeReader mbFn msg = reader $ maybe (fail msg) pure . mbFn
 
 
 main :: IO ()
 main = do
-  args <- getArgs
-  let packetsOnly = "--packets" `elem` args
-  let model       = if "--developer" `elem` args then Developer else Consumer
+  DumpArgs{ model, serial, packetsOnly, fromFile
+          } <- execParser $ info (helper <*> dumpArgs) (progDesc "Dumps Emotiv data")
 
-  devices <- getEmotivDevices
+  device <- case fromFile of
+    -- Use device file / file handle
+    Just f | Just s <- serial -> openEmotivDeviceFile model s f
+           | otherwise        -> error "A serial number must be provided when using --from-file"
 
-  putStrLn $ "Available devices:\n" ++ ppShow devices
+    -- Use HIDAPI
+    Nothing -> do
+      devices <- getEmotivDevices
 
-  device <- case devices of
-    [] -> error "No devices found."
-    _  -> openEmotivDevice model (last devices)
+      putStrLn $ "Available devices:\n" ++ ppShow devices
+
+      case devices of
+        [] -> error "No devices found."
+        _  -> openEmotivDevice model (last devices)
 
   forever $ do
     (state, packet) <- readEmotiv device
