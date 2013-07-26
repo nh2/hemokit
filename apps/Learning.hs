6ea348917700763e6b24df41ff3ca55ff55915cc
Niklas Hambüchen <mail@nh2.me>
2013-07-26 20:19:19 +0900

    Implement my own Bayes (not tested)

--- apps/Learning.hs
+++ apps/Learning.hs
@@ -1,73 +1,59 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE NamedFieldPuns #-}
-
-{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Learning where
 
-import HLearn.Algebra
+import Control.Applicative
+import Data.Map (Map)
+import Data.Maybe
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import HLearn.Algebra hiding (Map) -- TODO report
 import HLearn.Models.Distributions
-import HLearn.Models.Classifiers.Bayes
-import HLearn.Models.Classifiers.Common
-
--- import GHC.TypeLits
 
 
-data FftCase r = FftCase
-  { result :: r
-  , ffts   :: [Double]
-  } deriving (Read, Show, Eq, Ord)
+type FeatureIndex = Int
 
-toFftCase :: ([Double], r) -> FftCase r
-toFftCase (vals, res) = FftCase res vals
-
-
-instance Labeled (FftCase r) where
-  type Label      (FftCase r) = r
-  type Attributes (FftCase r) = FftCase r -- TODO change to [Double]
-  getLabel = result
-  getAttributes p = p
-
--- type Double462 = '[Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double]
-type Double231 = '[Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double]
-type Double10 = '[Double,Double,Double,Double,Double,Double,Double,Double,Double,Double]
-
-instance Trainable (FftCase r) where
-  -- Generated by "[" ++ (intercalate "," $ replicate 231 "Double") ++ "]
-  -- type GetHList (FftCase r) = HList (r ': Replicate 231 Double) -- needs UndecidableInstances
-  type GetHList (FftCase r) = HList '[r,Double,Double,Double,Double,Double,Double,Double,Double,Double,Double]
-  getHList FftCase{ result, ffts } = result ::: list2hlist ffts
+data BayesClassifier label = BayesClassifier -- label must be Ord, Enum
+  { distsByLabelAndFeature :: [(label, [Normal Double])]
+    -- ^ invariants:
+    -- * nonempty
+    -- * ordered by label
+    -- * all distribution lists have same size featuresSize
+  , featuresSize           :: Int
+  } deriving (Eq, Show)
 
 
-
-data TH_result = TH_result
-data TH_ffts   = TH_ffts
-
-instance TypeLens TH_result where type TypeLensIndex TH_result = Nat1Box Zero
-instance TypeLens TH_ffts   where type TypeLensIndex TH_ffts   = Nat1Box (Succ Zero)
-
-
-
-type BayesClassifier r = Bayes TH_result
-                               (Multivariate (FftCase r)
-                                 '[ MultiCategorical   '[r]
-                                  , Independent Normal Double10
-                                  ]
-                                  Double
-                               )
+check :: [([a], b)] -> ()
+check cases
+  | null cases                   = error "trainBayes: cannot train on empty cases list"
+  | any hasDifferentLength cases = error "trainBayes: feature vectors do not have same size"
+  | otherwise                    = ()
+  where
+    len                       = length $ fst (head cases)
+    hasDifferentLength (l, _) = length l /= len
 
 
-trainFFT :: (Ord r, Enum r) => [([Double], r)] -> BayesClassifier r
-trainFFT = train . map toFftCase
+trainBayes :: forall r . (Ord r, Enum r, Show r) => [([Double], r)] -> BayesClassifier r
+trainBayes cases | () <- check cases = BayesClassifier dists len
+  where
+    usedLabels = Set.toList . Set.fromList $ map snd cases -- ordered
 
-arbitraryResult :: (Enum r) => r
-arbitraryResult = toEnum 0
+    dists = [  (label, [ distFor (label, fi) | fi <- [0 .. len-1] ])  | label <- usedLabels ]
 
-classifyFFT :: (Ord r, Enum r) => BayesClassifier r -> [Double] -> r
-classifyFFT classifier c = classify classifier (getAttributes (FftCase arbitraryResult c))
+    m = train <$> inputsByLabelAndFeature :: Map (r, Int) (Normal Double)
 
-probabilitiesFFT :: (Ord r, Enum r) => BayesClassifier r -> [Double] -> [(r, Double)]
-probabilitiesFFT classifier c = dist2list $ probabilityClassify classifier (getAttributes (FftCase arbitraryResult c))
+    inputsByLabelAndFeature :: Map (r, Int) [Double]
+    inputsByLabelAndFeature = Map.fromListWith (++)
+                                [ ((label, fi), [val]) | (features, label) <- cases
+                                                       , (fi, val) <- zip [0..] features ]
 
+    len = length $ fst (head cases)
+    distFor k = fromMaybe (error $ "trainBayes: Missing (label x feature): " ++ show k)
+                          (Map.lookup k m)
+
+
+classifyBayes :: (Ord r, Enum r) => BayesClassifier r -> [Double] -> [(r, Double)]
+classifyBayes (BayesClassifier dists len) features
+  | length features /= len = error "classifyBayes: input feature vector not of same size as training ones"
+  | otherwise              = [ (label, pdf d val) | (label, featureDists) <- dists
+                                                  , (val, d) <- zip features featureDists ]
