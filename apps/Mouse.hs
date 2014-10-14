8239a907e6218d85a85ed3cb7c4064b3af2ece13
Niklas Hambüchen <mail@nh2.me>
2014-10-14 21:45:47 +0200

    hemokit-mouse: Depend on robot >= 1.3.0.1, drop xhb dependency.
    Fixes build, because newer robot versions contain a `connect` function
    that conflicted with xhb's `connect` function.

--- apps/Mouse.hs
+++ apps/Mouse.hs
@@ -3,7 +3,6 @@ module Main where
 import           Control.Concurrent (threadDelay, forkIO)
 import           Control.Monad
 import           Data.IORef
-import           Graphics.XHB.Connection (connect)
 import           System.Environment
 import           System.IO
 import           Test.Robot
@@ -26,17 +25,15 @@ main = do
   device <- openEmotivDevice model $ case reverse devices of d:_ -> d
                                                              []  -> error "no Epoc devices found"
 
-  m'xConnection <- connect
+  conn <- connect
   xy <- newIORef (0,0)
 
-  case m'xConnection of
-    Nothing -> return ()
-    Just xc -> void . forkIO . forever $ do
-      (x, y) <- readIORef xy
-      print (x, y)
-      writeIORef xy (0, 0)
-      runRobotWithConnection (moveBy ((-x) `quot` 10) (y `quot` 10)) xc
-      threadDelay 10000
+  void . forkIO . forever $ do
+    (x, y) <- readIORef xy
+    print (x, y)
+    writeIORef xy (0, 0)
+    runRobotWith conn (moveBy ((-x) `quot` 10) (y `quot` 10))
+    threadDelay 10000
 
   void $ untilNothing (readEmotiv device) $ \(state, _) -> do
 
